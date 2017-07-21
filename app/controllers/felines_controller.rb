56a8376ae8043bb34988e41185c6911d3c7fb09a
jin camou <jincamou@gmail.com>
2017-07-21 14:26:11 -0700

    create & add methods to felines controller; add & pass request spec for get felines route

---
+++ app/controllers/felines_controller.rb
@@ -0,0 +1,43 @@
+class FelinesController < ApplicationController
+  def index
+    @breed = Breed.find(params[:breed_id])
+    @felines = @breed.felines
+    json_response(@felines)
+  end
+
+  def show
+    @breed = Breed.find(params[:breed_id])
+    @feline = @breed.felines.find(params[:id])
+    json_response(@feline)
+  end
+
+  def create
+    @breed = Breed.find(params[:breed_id])
+    @feline = @breed.felines.create!(feline_params)
+    json_response(@feline, :created)
+  end
+
+  def update
+    @breed = Breed.find(params[:breed_id])
+    @feline = Feline.find(params[:id])
+    if @feline.update!(feline_params)
+      render status: 200, json: {
+        message: "Prrr"
+      }
+    end
+  end
+
+  def destroy
+    @feline = Feline.find(params[:id])
+    if @feline.destroy!
+      render status: 200, json: {
+        message: "Adopted!"
+      }
+    end
+  end
+
+  private
+  def feline_params
+    params.permit(:name, :age, :coat_color, :breed_id)
+  end
+end
---
+++ spec/requests/get_felines_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "get all felines route", :type => :request do
+  before do
+    Breed.destroy_all
+    breed = FactoryGirl.create(:breed_with_felines)
+    get "/breeds/#{breed.id}/felines/page/1"
+  end
+
+  it 'returns all felines' do
+    expect(JSON.parse(response.body).size).to eq(10)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
